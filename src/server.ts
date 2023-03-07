c77e096d5171a2599707e23ad6283d92dbb7c65b
Alexandre Santos <alexandre@Katarzynkas-MacBook-Pro.local>
2023-03-07 21:00:02 +0100

    this commit will setup  eslint

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
-const app = fastify();
+const app = fastify()
 
 app.get('/hey', () => {
   return 'Hey Seedtag XD'
 })
 
-app.listen({
-  port: 3333,
-}).then(() => { 
-  console.log("Server running on port 3333");
-});
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('Server running on port 3333')
+  })
